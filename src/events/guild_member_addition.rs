4dfa74277094553e722ff0db37c13079db4a3919
legendofmiracles <legendofmiracles@protonmail.com>
2021-03-18 20:29:42 +0000

    changes the widget color when the user is younger than 3 days

--- src/events/guild_member_addition.rs
+++ src/events/guild_member_addition.rs
@@ -1,4 +1,7 @@
 use super::*;
+use chrono::{DateTime, Utc};
+use std::time::SystemTime;
+
 pub async fn guild_member_addition(
     ctx: client::Context,
     guild_id: GuildId,
@@ -12,16 +15,24 @@ pub async fn guild_member_addition(
     config
         .channel_bot_traffic
         .send_embed(&ctx, |e| {
+            let date = new_member.user.created_at();
             e.author(|a| a.name("Member Join").icon_url(new_member.user.face()));
             e.title(new_member.user.name_with_disc_and_id());
             e.description(format!("User {} joined the server", new_member.mention()));
             e.field(
                 "Account Creation Date",
-                util::format_date_detailed(new_member.user.created_at()),
+                util::format_date_detailed(date),
                 false,
             );
             if let Some(join_date) = new_member.joined_at {
                 e.field("Join Date", util::format_date(join_date), false);
+            }
+            if date
+                .signed_duration_since(DateTime::<Utc>::from(SystemTime::now()))
+                .num_days()
+                <= 3
+            {
+                e.color(serenity::utils::Color::from_rgb(253, 242, 0));
             }
         })
         .await?;
