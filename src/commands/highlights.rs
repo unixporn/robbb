feb9e79c833bbf2e31749a3a9b0bb40a1a50cbda
legendofmiracles <legendofmiracles@protonmail.com>
2021-03-27 15:31:23 +0100

    better error handling

--- src/commands/highlights.rs
+++ src/commands/highlights.rs
@@ -7,9 +7,9 @@ use crate::Arc;
 pub async fn add_highlight(ctx: &client::Context, msg: &Message, args: Args) -> CommandResult {
     let args = args.message().trim().to_string();
     if args.contains(" ") {
-        abort_with!("Highlight can't contain a space for implementation/performance reasons");
+        abort_with!(UserErr::Other("Highlight can't contain a space for implementation/performance reasons".to_string()));
     } else if args.is_empty() {
-        abort_with!("You must provide a argument");
+        abort_with!(UserErr::InvalidUsage("You must provide a argument"));
     }
 
     let db: Arc<Db> = ctx.get_db().await;
@@ -27,7 +27,7 @@ pub async fn add_highlight(ctx: &client::Context, msg: &Message, args: Args) -> 
         .map(|(word, _)| word);
 
     if highlights_by_user.collect_vec().len() as u8 == n {
-        abort_with!(format!("You already set your {} highlights", n));
+        abort_with!(UserErr::Other(format!("You already set your {} highlights", n)));
     }
     db.set_highlight(msg.author.id, args.clone())
         .await
@@ -56,7 +56,7 @@ pub async fn get_highlights(ctx: &client::Context, msg: &Message) -> CommandResu
 
     // yes yes, we are checking the length of the text, whatever
     if highlights_text.len() == 0 {
-        abort_with!("You don't seem to have set any highlights");
+        abort_with!(UserErr::Other("You don't seem to have set any highlights".to_string()));
     } else {
         msg.reply_success(&ctx, highlights_text).await?;
     }
@@ -74,7 +74,7 @@ pub async fn remove_highlight(
     let db: Arc<Db> = ctx.get_db().await;
     args.quoted();
     let args = args.message().to_string();
-    db.remove_highlight(msg.author.id, args.clone()).await?;
+    db.remove_highlight(msg.author.id, args.clone()).await.user_error("Failed to remove the highlight.")?;
     msg.reply_success(
         &ctx,
         format!(
