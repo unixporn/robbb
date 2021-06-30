a63d0f8b30d878e83b2c24e842ec48af71d9082e
fortuneteller2k <lythe1107@gmail.com>
2021-06-30 21:23:36 +0800

    nixpkgs-fmt pass

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
-{ pkgs ? import <nixpkgs> {}  }:
+{ pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
-          
-          buildInputs = with pkgs; [ openssl sqlx-cli rustfmt clippy ];
-          shellHook = ''
-            export $(cat .env)
-          '';
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
+
+  buildInputs = with pkgs; [ openssl sqlx-cli rustfmt clippy ];
+  shellHook = ''
+    export $(cat .env)
+  '';
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
