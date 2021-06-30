15511976599258135507fb255a03a17d606061de
fortuneteller2k <lythe1107@gmail.com>
2021-06-30 21:25:31 +0800

    use packages instead of (native)buildInputs

--- shell.nix
+++ shell.nix
@@ -1,10 +1,21 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
 
-  buildInputs = with pkgs; [ openssl sqlx-cli rustfmt clippy ];
+pkgs.mkShell {
+  packages = with pkgs; [
+    rustc
+    cargo
+    rust-analyzer
+    gcc
+    pkg-config
+    openssl
+    sqlx-cli
+    rustfmt
+    clippy
+  ];
+
   shellHook = ''
     export $(cat .env)
   '';
+
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
