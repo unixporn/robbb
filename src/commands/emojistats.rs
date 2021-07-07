07bda94da81758e1bfa90f292d03d9bafad54740
AusCyber <willp@outlook.com.au>
2021-07-07 15:55:15 +1000

    fixup! Changed to only show all emojis

--- src/commands/emojistats.rs
+++ src/commands/emojistats.rs
@@ -1,4 +1,3 @@
-use serenity::http::CacheHttp;
 use std::collections::hash_map::HashMap;
 
 use super::*;
@@ -87,28 +86,3 @@ fn display_emojis(
         })
         .join("\n")
 }
-
-//fn sort_emojis(
-//    &mut args: Args,
-//    emojis: impl Iterator<Item = EmojiData>,
-//) -> Result<(DisplayMode, OrderingMode, impl Iterator<Item = EmojiData>)> {
-//    let mode = args.single_quoted::<String>().ok();
-//
-//    let reactions_or_in_text = args.single_quoted::<String>().ok();
-//
-//    //    let part1 =match mode {
-//    //        Some(x) if x == "in_text" => return Ok((DisplayMode::InText,OrderingMode::Top,emojis.sorted_by(|a, b| (a.in_text).cmp(&b.in_text)))),
-//    //        Some(x) if x == "reactions" => emojis.sorted_by(|a, b| (a.reactions).cmp(&b.reactions)),
-//    //        Some(x) if x == "top" => {
-//    //            emojis.sorted_by(|a, b| (a.reactions + a.in_text).cmp(&(b.reactions + b.in_text)))
-//    //        }
-//    //        Some(x) if x == "bottom" => emojis
-//    //            .sorted_by(|a, b| (a.reactions + a.in_text).cmp(&(b.reactions + b.in_text)))
-//    //            .rev(),
-//    //        Some(x) => {
-//    //                let emoji  = crate::util::find_emojis(x).first()?;
-//    //
-//    //            }
-//    //        _ => emojis.sorted_by(|a, b| (a.reactions + a.in_text).cmp(&(b.reactions + b.in_text))),
-//    //    }
-//}
