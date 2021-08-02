ef7945e486a06cd55e5b3a44fa57892fef5acba9
elkowar <5300871+elkowar@users.noreply.github.com>
2021-08-02 13:33:58 +0200

    Add automatic dehoisting of members on server start and member update

---
+++ src/events/guild_member_update.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+static HOISTING_CHAR: &[char] = &['!', '"', '$', '\'', '(', ')', '*', '-', '.', '/', '='];
+
+pub async fn guild_member_update(
+    ctx: client::Context,
+    _old: Option<Member>,
+    new: Member,
+) -> Result<()> {
+    dehoist_member(ctx, new).await?;
+    Ok(())
+}
+
+pub async fn dehoist_member(ctx: client::Context, member: Member) -> Result<()> {
+    let display_name = member.display_name();
+    if !display_name.starts_with(HOISTING_CHAR) {
+        return Ok(());
+    }
+    let cleaned_name = display_name.trim_start_matches(HOISTING_CHAR);
+    member
+        .edit(&ctx, |edit| edit.nickname(cleaned_name))
+        .await
+        .with_context(|| format!("Failed to rename user {}", member.user.tag()))?;
+    Ok(())
+}
