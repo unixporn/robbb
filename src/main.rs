24206d1703b1e2405be681be6d12142d79ad7ca2
elkowar <5300871+elkowar@users.noreply.github.com>
2021-10-13 08:23:07 +0200

    Make use of discord timestamps wherever time is shown

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,7 @@ pub struct Config {
     pub attachment_cache_path: PathBuf,
     pub attachment_cache_max_size: usize,
 
-    pub start: chrono::DateTime<chrono::Utc>,
+    pub time_started: chrono::DateTime<chrono::Utc>,
 }
 
 impl Config {
@@ -95,7 +95,7 @@ impl Config {
             channel_tech_support: ChannelId(parse_required_env_var("CHANNEL_TECH_SUPPORT")?),
             attachment_cache_path: parse_required_env_var("ATTACHMENT_CACHE_PATH")?,
             attachment_cache_max_size: parse_required_env_var("ATTACHMENT_CACHE_MAX_SIZE")?,
-            start: chrono::Utc::now(),
+            time_started: chrono::Utc::now(),
         })
     }
 
