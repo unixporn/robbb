40bbcbd47b268ca04e551740c41aeaa96e1901f0
elkowar <5300871+elkowar@users.noreply.github.com>
2022-05-01 15:06:00 +0200

    Fix spoilered image quoting also excluding images starting with SPOILER without the underscore

--- src/events/message_create.rs
+++ src/events/message_create.rs
@@ -328,7 +328,7 @@ async fn handle_quote(ctx: &client::Context, msg: &Message) -> Result<bool> {
     let image_attachment = mentioned_msg
         .attachments
         .iter()
-        .filter(|x| !x.filename.starts_with("SPOILER"))
+        .filter(|x| !x.filename.starts_with("SPOILER_"))
         .find(|x| x.dimensions().is_some());
 
     debug!("retrieved the mentioned message");
