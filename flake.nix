99d95a507ffbb92411a01cbbc8d32cda13ddec9d
Tanish2002 <tanishkhare@gmail.com>
2022-07-21 17:35:29 +0530

    Modiy nix Files
    * did a `cargo update`
    * did some `cargo sqlx` magic and probably broke base.db, idk :p
    * updated `shell.nix` `default.nix` and `flake.nix`
    * We can now use `nix build` to build robbb with all checks(clippy,
    cargofmt, etc) and build all the cargo deps separately so they can be
    re-used. Reproducible builds!
    * We can also use `nix run` to build just like `nix build` and run the
    built package
    * `nix-shell` and `nix-build` should just work like before, they
    use flakes.lock now so removed the sources.nix and sources.json files.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,72 @@
 {
-  description = "r/unixporn discord bot";
 
+  description = "r/unixporn discord bot";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system}; in
-    {
-      devShell = import ./shell.nix { inherit pkgs; };
-    }
-  );
+  outputs = { self, nixpkgs, utils, crane, rust-overlay, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust-toolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+        commonArgs = {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ rust-toolchain pkg-config ];
+          buildInputs = with pkgs; [
+            openssl
+            sqlx-cli
+            rust-analyzer
+            sqlitebrowser
+            sqlite
+          ];
+        };
+        # Build Deps so We don't have to build them everytime
+        cargoArtifacts =
+          craneLib.buildDepsOnly (commonArgs // { pname = "robbb-deps"; });
+        # Run Clippy
+        robbbClippy = craneLib.cargoClippy (commonArgs // {
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "-- -D warnings";
+        });
+        robbb = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          pname = "robbb";
+        });
+      in {
+        # Run Checks
+        checks = { inherit robbb robbbClippy; };
+        # `nix build`
+        packages.default = robbb;
+
+        # `nix run`
+        apps.default = utils.lib.mkApp { drv = robbb; };
+
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.checks;
+
+          shellHook = ''
+            export $(cat .env)
+          '';
+          # Extra inputs can be added here
+          inherit (commonArgs) nativeBuildInputs;
+          inherit (commonArgs) buildInputs;
+        };
+      });
 }
