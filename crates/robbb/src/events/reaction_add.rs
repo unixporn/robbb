c5e7414fc2af82d866e88e0ff23f1eed0e86df07
ElKowar <5300871+elkowar@users.noreply.github.com>
2024-01-17 15:03:07 +0100

    Remove poll reaction handling feature

--- crates/robbb/src/events/reaction_add.rs
+++ crates/robbb/src/events/reaction_add.rs
@@ -8,31 +8,7 @@ pub async fn reaction_add(ctx: client::Context, event: Reaction) -> Result<()> {
         return Ok(());
     }
     let msg = event.message(&ctx.http).await?;
-
-    let is_poll = msg.author.bot
-        && msg
-            .embeds
-            .iter()
-            .any(|embed| embed.title.as_ref().map(|x| x.starts_with("Poll")).unwrap_or(false));
-
-    if is_poll {
-        // This is rather imperfect, but discord API sucks :/
-        // we're pretty much deleteing all other reactions and are giving it the user to delete the reaction from,
-        // such that discord API knows which of the reactions to remove. If the user hasn't reacted
-        // with that emote, it'll error, but we don't really care :/
-        println!("message reactions: {:?}", msg.reactions);
-        for r in &msg.reactions {
-            if r.reaction_type != event.emoji {
-                tracing::debug!(reaction.typ = ?r.reaction_type, reaction.user_id = %user.id, msg.id = %msg.id, "Removing reaction to poll");
-                log_error!(
-                    ctx.http
-                        .delete_reaction(msg.channel_id, msg.id, user.id, &r.reaction_type)
-                        .await
-                );
-            }
-        }
-    }
-    if !is_poll && msg.reactions.iter().any(|x| x.reaction_type == event.emoji && x.count == 1) {
+    if msg.reactions.iter().any(|x| x.reaction_type == event.emoji && x.count == 1) {
         handle_reaction_emoji_logging(ctx, event).await?;
     }
     Ok(())
